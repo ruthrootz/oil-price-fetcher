cebe3dd6b36de3d7cc9d8adb027fdbb9cdd5c7e8
Ruth Oldja <roldja@foundationsoft.com>
2020-07-14 14:54:29 -0400

    added kivy

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import requests
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
 
-url = 'https://api.oilpriceapi.com/v1/prices/latest'
-headers = {
-    'Authorization': 'Token b7ed054ba6e6d513af611db4e0b0e7f2',
-    'Content-Type': 'application/json'
-}
+kivy.require('1.11.1')
 
-response = requests.get(url=url, headers=headers)
-data = response.json()
-print(data)
+
+class OilApp(App):
+    def build(self):
+        url = 'https://api.oilpriceapi.com/v1/prices/latest'
+        headers = {
+            'Authorization': 'Token b7ed054ba6e6d513af611db4e0b0e7f2',
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.get(url=url, headers=headers)
+        data = response.json()
+        return Label(text='current oil price: \n' + str(data["data"]["formatted"]))
+
+
+if __name__ == '__main__':
+    OilApp().run()
